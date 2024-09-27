fe981ac58056ed2bbc36630b73374fe5d1da11bf
ananyaamzare <ananyaamzare30@gmail.com>
2024-09-27 15:20:40 +0530

    variable
    variable

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+
+var myName; //variable Declaration
+myName = "Ananya"; //variable Assingment or Initilazation
+
+console.log(myName);
+
+var city = "pune";
+console.log(city);
+
+var pin // var declasration
+console.log(pin); // undefined 
