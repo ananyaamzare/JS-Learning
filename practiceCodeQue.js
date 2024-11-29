703464862a4d9805fe67eab95c3e33965dc38dc7
ananyaamzare <ananyaamzare30@gmail.com>
2024-11-29 18:21:43 +0530

    practice code
    some important code

--- practiceCodeQue.js
+++ practiceCodeQue.js
@@ -1,37 +1,79 @@
-// vowelCount
-function vowelCount(str){
+// // vowelCount
+// function vowelCount(str){
 
-    let count = 0;
-    for (char of str){
-        if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u"){
-            count ++;
-        }
-    }
-    console.log(`totalCount = ${count}`);
-    }
-vowelCount("i am very good ui developer");
+//     let count = 0;
+//     for (char of str){
+//         if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u"){
+//             count ++;
+//         }
+//     }
+//     console.log(`totalCount = ${count}`);
+//     }
+// vowelCount("i am very good ui developer");
 
 
 
 
 // your code goes here
-let input='';
-process.stdin.on("data",function(chunk){
-    input+=chunk;
-})
-process.stdin.on("end",function(){
-    main(input)
-})
-function main(input){
-    let lines=input.trim().split("\n");
-    let test=Number(lines[0])
-    for (let i=0;i<lines.length;i+=3){
-        let [n,m]=lines.split(" ").map(Number)
-        let str1=lines[i+1]
-        let str2=lines[i+2]
-    }
+// let input='';
+// process.stdin.on("data",function(chunk){
+//     input+=chunk;
+// })
+// process.stdin.on("end",function(){
+//     main(input)
+// })
+// function main(input){
+//     let lines=input.trim().split("\n");
+//     let test=Number(lines[0])
+//     for (let i=0;i<lines.length;i+=3){
+//         let [n,m]=lines.split(" ").map(Number)
+//         let str1=lines[i+1]
+//         let str2=lines[i+2]
+//     }
     
+// }
+
+
+//duplicate number 
+
+// const array = [1, 3, 4, 5, 6, 54, 3, 2, 54, 4];
+
+// let duplicate = [];
+
+// for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//         if (array[i] === array[j] && !duplicate.includes(array[i])) {
+//             duplicate.push(array[i]);
+//         }
+//     }
+// }
+// console.log(duplicate);
+
+// const array = [1,2,1,2,3,4,3,4,5,6,5,6];
+
+// let duplicateArray =[];
+
+// for(let i=0; i<array.length; i++) {
+
+//     for(j=i+1; j<array.length; j++){
+
+//         if(array[i] === array[j] && !duplicateArray.includes(array[i])) {
+//              duplicateArray.push(array[i]);
+//         }
+//         }
+//     }
+// console.log(duplicateArray);
+
+function nonRepeat(str){
+
+    for(char of str) {
+       
 }
+}
+
+
+
+
 
 
 
